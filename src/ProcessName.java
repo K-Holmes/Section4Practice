30b457df7bef06582d1566cb7cb7763650683ce0
Kayla Holmes <Kayla.Holmes@cdpipelinedevshop>
2022-08-23 09:28:06 -0400

    "Finished Activities"

--- src/ProcessName.java
+++ src/ProcessName.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
-
 public class ProcessName {
 
 
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.print("Enter your name: ");
+        String fullName = console.nextLine();
+        int spacing = fullName.indexOf(' ');
+        String firstName = fullName.substring(0, spacing);
+        String firstInitial = firstName.substring(0, 1);
+        String lastName = fullName.substring(spacing+1);
+        String newName = lastName + ", " + firstInitial + ".";
 
-
-        // your code goes here
-
-
+        System.out.println("Your name is: " + newName);
     }
 }
--- src/TestClass.java
+++ src/TestClass.java
@@ -1,10 +1,11 @@
 public class TestClass {
 
     public static void main(String args[]) {
-        ComputeMethods com = new ComputeMethods();
-        ComputeMethods.roll();
-        ComputeMethods.fToC(65);
-        ComputeMethods.hypotenuseSolve(4, 5);
+        ComputeMethods dem = new ComputeMethods();
+        System.out.println(dem.fToC(76));
+        System.out.println(dem.hypotenuseSolve(4, 5));
+        System.out.println(dem.roll());
+
 
         //Create an instance of ComputeMethods,
         //invoke the 3 methods and display their results
